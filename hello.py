d5d27b73a6148e682464bc1a7bf888a97e4eff67
ponyslaystation25 <junkm0144@gmail.com>
2025-06-20 20:11:43 +0200

    Commit after finishing the migration of files to the networthcalculator directory and adding new files.

--- hello.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-def greet(name):
-    return "Hello " + name + "!"
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-
-demo.launch(server_name="localhost", server_port=7861)
